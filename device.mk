21e72709d1e415c546aa2a1ef1f3da14e74ccf3e
Szymon Śliwiński <100Daisy@protonmail.com>
2020-07-19 11:06:26 +0200

    Add PixelLIveWallpaper's

--- device.mk
+++ device.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
     libvulkan \
     libtinyxml
 
+# Wallpapers
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
